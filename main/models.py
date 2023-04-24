9c88c60e2e3c07decf97288310b570e4ed086358
Иван Афанасов <afanasov711@gmail.com>
2023-04-24 10:54:25 +0300

    base

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=200, null=True)
+    password = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.email
