44da0e1008ee206bfaf9080c079fdd2b57a24439
Иван Афанасов <afanasov711@gmail.com>
2023-05-21 20:31:34 +0300

    21.05

--- main/forms.py
+++ main/forms.py
@@ -4,12 +4,16 @@ from main.models import User
 
 class MainForm(forms.ModelForm):
 
+   
+    password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = '__all__'
 
-    # def __init__(self, *args, **kwargs):
-    #     super(MainForm, self).__init__(*args, **kwargs)
-    #     self.fields['myfield'].widget.attrs.update({'class': 'myfieldclass'})
+    def __init__(self, *args, **kwargs):
+        super(MainForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        
 
     
