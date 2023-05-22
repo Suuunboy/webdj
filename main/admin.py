07676bdaf542b224d45b20c95fe2919126723f1f
Иван Афанасов <afanasov711@gmail.com>
2023-05-22 19:28:02 +0300

    aaaaaaaa

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from main.models import User
+from main.models import Post
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Post)
--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    likes = models.ManyToManyField(User, related_name='blog_post')
+
+    def __str__(self):
+        return self.title
